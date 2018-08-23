824b94567312c084047afaa423780ac19acceaee
Gian Mina <arcanemindtraveler@gmail.com>
2018-08-24 03:48:21 +0800

    Reformatted code.

--- script.js
+++ script.js
@@ -1,198 +1,170 @@
-// Enable pusher logging - don't include this in production
-Pusher.logToConsole = true;
+const SENSOR_HEIGHT = 18; // In centimeters
 
-var tv = 1000;
+const COLOR_NORMAL = "#388e3c";
+const COLOR_WARNING = "#ef6c00";
+const COLOR_DANGER = "#d32f2f";
+const COLOR_ERROR = "#1565c0";
 
-var pusher = new Pusher('acd7c7e6fa6ab1b860d1', {
-  cluster: 'ap1',
-  encrypted: true
-});
+const WARNING_NORMAL = "NORMAL";
+const WARNING_WARNING = "WARNING";
+const WARNING_DANGER = "DANGER";
+const WARNING_ERROR = "ERROR";
 
+const WARNING_INFO_NORMAL = "Please keep calm and be vigilant.";
+const WARNING_INFO_WARNING = "Please prepare to evacuate.";
+const WARNING_INFO_DANGER = "Please evacuate immediately!";
+const WARNING_INFO_ERROR = "The water levels displayed may be incorrect.";
+const WARNING_INFO_OUTDATED = "The last recorded water levels are displayed.";
+
+var receivedDistance = SENSOR_HEIGHT;
+var distance = SENSOR_HEIGHT;
 var updated = false;
-var initial = 18;
-var distance = initial;
-var color = 'blue';
-var warning = '';
-//var currDate = new Date();
+var chartColor = COLOR_NORMAL;
+var chart = null;
 
-/* 
-var channel = pusher.subscribe('distance');
-channel.bind('flood-reading', function(data) {
-  //alert(JSON.stringify(data));
-    //addRandomData(chart);
-    distance = parseFloat(data.message) // Check this line if there are errors. Change data.message -> data["message"]
-    addNewData(chart, distance);
+var mock = false;
+
+window.onload = function() {
+  Pusher.logToConsole = mock;
+  
+  var pusher = new Pusher('acd7c7e6fa6ab1b860d1', {
+      cluster: 'ap1',
+      encrypted: true
+  });
+  
+  var channel = pusher.subscribe('distance');
+  channel.bind('flood-reading', function(data) {
+      receivedDistance = parseFloat(data.message) // Check this line if there are errors. Change data.message -> data["message"]
+      updated = true;
+  });
+
+  initializeGraph();
+  
+  setInterval(function () {
+    var warning = "";
+    var warningInfo = "";
+    var waterLevel = 0;
+    var lastDistance = distance;
+  
+    if (mock) {
+      var update = getRandomUpdate(0.1, 5, 0.5);
     
-    if (distance >= 0 && distance < 50)
-      color = 'red';
-    else if (distance >= 50 && distance < 100)
-      color = 'orange';
-     else if (distance >= 100)
-      color = 'green';
-    else
-      color = 'blue';
+      distance = distance + update;
       
-    chart.series[0]["color"] = color;
+      updated = true;
+    } else
+      distance = receivedDistance;
+  
+    if (!updated) {
+      distance = lastDistance;
+      warningLevel = -1;
+    } else {
+      waterLevel = SENSOR_HEIGHT - distance;
+      
+      if (waterLevel >= 0 && waterLevel < 6)
+        warningLevel = 0;
+      else if (waterLevel >= 6 && waterLevel < 12)
+        warningLevel = 1;
+      else if (waterLevel >= 12)
+        warningLevel = 2;
+      else
+        warningLevel = -1;
+    }
+  
+    document.getElementById("warning").classList = "large";
     
-    console.log(chart);
+    switch (warningLevel) {
+      case 0:
+        chartColor = COLOR_NORMAL;
+        warning = WARNING_NORMAL;
+        warningInfo = WARNING_INFO_NORMAL;
+  
+        document.getElementById("warning").classList.add("warning-normal");
+        break;
+      case 1:
+        chartColor = COLOR_WARNING;
+        warning = WARNING_WARNING;
+        warningInfo = WARNING_INFO_WARNING;
+  
+        document.getElementById("warning").classList.add("warning-warning");
+        break;
+      case 2:
+        chartColor = COLOR_DANGER;
+        warning = WARNING_DANGER;
+        warningInfo = WARNING_INFO_DANGER;
+  
+        document.getElementById("warning").classList.add("warning-danger");
+        break;
+      case -1:
+        chartColor = COLOR_ERROR;
+        warning = WARNING_ERROR;
+        warningInfo = WARNING_INFO_ERROR;
+  
+        document.getElementById("warning").classList.add("warning-error");
+        break;
+    }
     
+    if (!updated)
+      warningInfo = WARNING_INFO_OUTDATED;
+
+    document.getElementById("water-level").textContent = Math.round(waterLevel) + " cm";
+    document.getElementById("warning").textContent = warning;
+    document.getElementById("warning-info").textContent = warningInfo;
+  
+    if (updated)
+      addNewData(chart, waterLevel);
+    
+    chart.series[0].color = chartColor;
     chart.render();
-    
-    updated = true;
-});
- */
 
-setInterval(function () {
-  //addRandomData(chart);
-  var weight = 0.1;
-  
-  if (Math.random(100) < 0.5)
-  	weight = weight * 10;
-  
-  var update = (Math.random() * weight) * (Math.floor(Math.random() * 3) - 1);
-  if ((distance >= initial && update > 0) || (distance <= 0 && update < 0))
-  	update *= -1;
-    
-  if (distance + update < 0)
-  	update *= -1;
-  //console.log(update);
-    
-  distance += update;
-  
-  
-  //console.log(chart);
-  //distance += Math.random() * 1;
-  
-  //distance = 1 / distance;
-  //console.log(distance);
-  
-  var floodLevel = initial - distance;
-  
-  
-  document.getElementById("value").textContent = Math.round(floodLevel) + " cm";
-  	
-  //console.log((Math.random() * 10) * (Math.floor(Math.random() * 3) - 1));
-  
-  updated = true;
-  /* 
-  if (!updated)
-    distance = 0.0;
-   */
-  addNewData(chart, floodLevel);
-  
-  if (floodLevel >= 0 && floodLevel < 6) {
-    color = '#388e3c';
-    warning = 'NORMAL';
-    document.getElementById("warning").classList.remove("warning-danger");
-    document.getElementById("warning").classList.remove("warning-warning");
-    document.getElementById("warning").classList.add("warning-normal");
-    
-    document.getElementById("notice").textContent = "Please keep calm and be vigilant.";
-    
-  }
-  else if (floodLevel >= 6 && floodLevel < 12) {
-    color = '#ef6c00';
-    warning = 'WARNING';
-    document.getElementById("warning").classList.remove("warning-danger");
-    document.getElementById("warning").classList.remove("warning-normal");
-    document.getElementById("warning").classList.add("warning-warning");
-    
-    document.getElementById("notice").textContent = "Please prepare to evacuate.";
-  }
-  else if (floodLevel >= 12) {
-    color = '#d32f2f';
-    warning = 'DANGER';
-    document.getElementById("warning").classList.remove("warning-warning");
-    document.getElementById("warning").classList.remove("warning-normal");
-    document.getElementById("warning").classList.add("warning-danger");
-    
-    document.getElementById("notice").textContent = "Please evacuate immediately!";
-  }
-  else {
-    color = '#1565c0';
-    warning = 'ERROR';
-  }
-  
-  document.getElementById("warning").textContent = warning;
+    updated = false;
 
-  chart.series[0]["color"] = color;
+  }, 1000);
+}
 
-  updated = false;
-  
-  //chart = document.getElementsByClassName("app")[0].offsetWidth;
-
-  chart.render();
-
-}, tv);
-
-// Chart
-
-
-var chart = new Rickshaw.Graph({
+function initializeGraph() {
+  chart = new Rickshaw.Graph({
     element: document.querySelector("#chart"),
     width: "600",
     height: "300",
     renderer: "line",
-    max: initial,
+    max: SENSOR_HEIGHT,
     series: [{
-    	data: [],
-      color: color
+      data: [],
+      color: chartColor
     }]
-});
+  });
+  chart.render();
 
-var seriesData = chart.series[0].data;
-/* 
-for (var i = 0; i < 45; i++) {
-  seriesData.push({x: -1, y: 0});
-}
- */
-chart.render();
-
-var xAxis = new Rickshaw.Graph.Axis.X({
+  var xAxis = new Rickshaw.Graph.Axis.X({
     graph: chart,
     tickFormat: function(x) {
-    	var currDate = new Date(x * 1000);
+      var currDate = new Date(x * 1000);
       return currDate.getHours() + ":" + currDate.getMinutes() + ":" + currDate.getSeconds();
     }
-     /* 
-         timeUnit: function() {
-      var time = new Rickshaw.Fixtures.Time.Local();
-      time.formatTime = function(d) { 
-        return moment(d).format("HH:mm:ss"); 
-           }
-      
-      return time.unit("second");
-         }
-     */
-});
+  });
+  xAxis.render();
 
-xAxis.render();
-
-
-var yAxis = new Rickshaw.Graph.Axis.Y({
+  var yAxis = new Rickshaw.Graph.Axis.Y({
     graph: chart,
     tickFormat: Rickshaw.Fixtures.Number.formatKMBT
-});
+  });
+  yAxis.render();
+}
 
-yAxis.render();
-
-
-
-/* 
-var xhttp = new XMLHttpRequest(); 
-xhttp.onreadystatechange = function() {
-    addNewData(chart, this.responseText);
-    chart.render();
-};
-xhttp.open("GET", "ip of server", true);
-xhttp.send();
- */
- 
-function addRandomData(chart) {
-    var data = {
-        one: Math.floor(Math.random() * 40) + 120
-    };
-    chart.series.addData(data);
+function getRandomUpdate(weight, spikeMultiplier, spikeChance) {
+  if (Math.random(1.0) >= spikeChance)
+    weight = weight * spikeMultiplier;
+  
+  var update = (Math.random() * weight) * (Math.floor(Math.random() * 3) - 1);
+  
+  if ((distance >= SENSOR_HEIGHT && update > 0) || (distance <= 0 && update < 0))
+    update *= -1;
+    
+  if (distance + update < 0)
+    update *= -1;
+  
+  return update;
 }
 
 function addNewData(chart, value) {
@@ -201,17 +173,13 @@ function addNewData(chart, value) {
     		x: currDate.getTime() / 1000,
     		y: value,
     }
-    //chart.series.addData(data);
     
     var seriesData = chart.series[0].data;
     
-    if (seriesData.length >= 45)
+    if (seriesData.length >= 60)
     	seriesData.shift();
       
     seriesData.push(data);
     
     chart.update();
-     
-    console.log(seriesData);
-    
 }
